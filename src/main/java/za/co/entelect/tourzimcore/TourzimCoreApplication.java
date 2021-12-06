bb2097d05d4a617411c2d6f91c5ac7dc22e53efe
Gondai Mgano <gondaimgano@Gondais-MacBook-Air.local>
2021-12-06 12:00:38 +0200

    Initial commit

---
+++ src/main/java/za/co/entelect/tourzimcore/TourzimCoreApplication.java
@@ -0,0 +1,13 @@
+package za.co.entelect.tourzimcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourzimCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TourzimCoreApplication.class, args);
+	}
+
+}
